c571bf228997c7d3489c7b3533922726961c4c85
unknown <press1998@gmail.com>
2020-08-10 12:34:34 +0530

    Added validation for email

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x
 echo "Welcome to user registration problem"
 
-pattern="^[[:upper:]]{1}[a-zA-Z]{3,}$"
-read -p "Enter a Last Name: " lastName
-if [[ $lastName =~ $pattern ]]
+pattern="^[a-zA-Z0-9]{3,15}(|[.|_|%|+|-]?[a-zA-Z0-9]+)@[a-zA-Z0-9]{1,15}(.[a-z]{2,4})(|[.]?[a-z]{2,4})$"
+read -p "Enter a Email : " email
+if [[ $email =~ $pattern ]]
 then
 	echo "Valid"
 else
